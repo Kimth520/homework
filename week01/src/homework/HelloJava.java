f8aa898d1cb406f837b9f32f199125556bad8b45
Kimth520 <rlaxoqkdrn520@gmail.com>
2024-11-25 09:19:26 +0900

    241125 1주차 주말 과제

---
+++ week01/src/homework/HelloJava.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class HelloJava {// 클래스의 중괄호 시작영역
+	public static void main(String[] args) {//main 메소드의 중괄호 시작영역
+		System.out.println("HelloJava");//HelloJava 라는 값을 콘솔창에 출력하고 줄바꿈
+	}//main 메소드의 중괄호 끝 영역
+}//클래스의 중괄호 끝 영역
---
+++ week01/src/homework/PrintInfo.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class PrintInfo {//클래스의 중괄호 시작 영역
+	public static void main(String[] args) { //main 메소드의 중괄호 시작 영역 
+	System.out.println("이름: 김태현 / 나이: 26세"); // 이름과 나이를 콘솔창에 출력하고 줄바꿈
+	}//main 메소드의 중괄호 끝 영역
+}//클래스의 중괄호 끝 영역
