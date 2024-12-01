d7af26020b5716a6996303e489febc53b09bcb83
Kimth520 <https://github.com/Kimth520/homework.git>
2024-12-01 23:28:48 +0900

    2024 11/24 주말과제

--- week01/src/homework/PrintInfo2.java
+++ week01/src/homework/PrintInfo2.java
@@ -2,7 +2,7 @@ package homework;
 
 public class PrintInfo2 {
 	public static void main(String[] args) {
-		System.out.print("2024년 11월 24일 ");
+		System.out.print("2024년 11월 24일 ");//띄어쓰기 없
 		System.out.print("14시 40분");
 	}
 }
